17fad186286d2a8e41ae360482ce4b1c4b4a6fe7
tangrenyue <724562793@qq.com>
2019-05-24 23:39:55 +0800

    react

---
+++ react-study/src/App.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NameCard from './components/NameCard';
+import LikesButton from './components/LikesButton';
+import DigitalClock from './components/DigitalClock';
+
+const tags = ['react', 'my-page'];
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <DigitalClock></DigitalClock>
+                <LikesButton></LikesButton>
+                <NameCard name="Tom" number={446473876543} isHuman tags={tags}/>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default App;
